7d06c466b3331818a065fd21e137321b3e0c9ae3
Thierry <thierry.beiko@polymtl.ca>
2020-11-02 22:01:50 -0500

    Added all the definitions needed for setup

--- SpeakerSelector/SpeakerSelector.ino
+++ SpeakerSelector/SpeakerSelector.ino
@@ -1,19 +1,49 @@
-#include <Adafruit_GFX.h>
-#include <Adafruit_TFTLCD.h>
-#include <stdint.h>
-#include <TouchScreen.h>
-#include <MCUFRIEND_kbv.h>
+///////////////
+//
+// Sketch: SpeakerSelector - With TFT SPFD5408 TouchScreen
+// Author: Thierry Beiko - thierry_beiko@hotmail.com
+//
+// Versions:
+//    - 0.1   First Iteration
+// Comments: 
+//    The calibration of the touchscreen has to be done beforehand.
+//    The resulting values need to be modified in the definition section.
+//
+///////////////
 
+// library SPFD5408 - from https://github.com/JoaoLopesF/SPFD5408
+#include <SPFD5408_Adafruit_GFX.h>    // Core graphics library
+#include <SPFD5408_Adafruit_TFTLCD.h> // Hardware-specific library
+#include <SPFD5408_TouchScreen.h>     // Touch library
+
+// LCD Pin
 #define LCD_CS A3
 #define LCD_CD A2
 #define LCD_WR A1
 #define LCD_RD A0
 #define LCD_RESET A4
+
+// Calibrates value
+#define SENSIBILITY 300
+#define MINPRESSURE 10
+#define MAXPRESSURE 1000
+
+// Pins for the shield (Touchscreen)
+#define YP A2 
+#define XM A3 
+#define YM 8 
+#define XP 9 
+
+// Init TouchScreen
+TouchScreen ts = TouchScreen(XP, YP, XM, YM, SENSIBILITY);
+
 // Values obtained from the calibration
 #define SCREEN_MINX 121
 #define SCREEN_MINY 107
 #define SCREEN_MAXX 968
 #define SCREEN_MAXY 913
+
+// Assigning human-readable names to some common 16-bit color values
 #define BLACK       0x0000
 #define BLUE        0x001F
 #define RED         0xF800
@@ -33,14 +63,35 @@
 #define LIGHTGREY   0xC618
 #define DARKGREY    0x7BEF
 
-MCUFRIEND_kbv tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
+// Init LCD
+Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
+
+// Dimensions
+uint16_t width = 0;
+uint16_t height = 0;
+
+// Buttons
+#define BUTTONS 7
+#define BUTTON_ZONE_1_UP 0
+#define BUTTON_ZONE_1_DOWN 1
+#define BUTTON_ZONE_2_UP 2
+#define BUTTON_ZONE_2_DOWN 3
+#define BUTTON_ZONE_3_UP_IN 4
+#define BUTTON_ZONE_3_UP_OUT 5
+#define BUTTON_ZONE_3_DOWN 6
+
+Adafruit_GFX_Button buttons[BUTTONS];
+
+uint16_t buttons_y = 0;
 
 void setup(void) {
-  Serial.begin(9600);
-  uint16_t identifier = tft.readID();
-  tft.begin(identifier);
-  tft.fillScreen(CYAN);
-  Serial.println(tft.width());
+  // LCD setup
+  tft.reset();
+  tft.begin(0x9341);
+  tft.setRotation(1); // Change for desired rotation - 1 for landscape view with LCD reset button on the right
+
+  width = tft.width() - 1;
+  height = tft.height() - 1;
 
 }
 void loop(void) {
@@ -51,3 +102,30 @@ void loop(void) {
   
 
 } 
+
+void ButtonsInit() {
+
+  uint16_t x = 40;
+  uint16_t y = height - 20;
+  uint16_t w = 70;
+  uint16_t h = 50;
+  
+  uint8_t spacing_x = 10;
+  
+  uint8_t textSize = 2;
+
+  char buttonlabels[3][20] = {"Clear", "Show", "Recalib."};
+  uint16_t buttoncolors[15] = {RED, BLUE, RED};
+
+  for (uint8_t b=0; b<3; b++) {
+    buttons[b].initButton(&tft,                           // TFT object
+                  x+b*(w+spacing_x),  y,                  // x, y,
+                  w, h, WHITE, buttoncolors[b], WHITE,    // w, h, outline, fill, 
+                  buttonlabels[b], textSize);             // text
+  }
+
+  // Save the y position to avoid draws
+  
+  buttons_y = y;
+  
+}
